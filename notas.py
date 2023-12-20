a7a89a2e42239a56ab2b2b11e67431890bc0998b
joao-calvo <112580072+joao-calvo@users.noreply.github.com>
2023-12-20 10:52:42 -0400

    Add files via upload

--- notas.py
+++ notas.py
@@ -7,18 +7,18 @@ total = 0
 contador_notas = 0
 
 while contador_notas <= 9:
-	contador_notas += 1
+	contador_notas += 1 # Adicionando incremento para a variável
 	try:
-		nota = float(input(f"Informe a nota da Atividade {contador_notas}: "))
+		nota = float(input(f"Informe a nota da Atividade {contador_notas}: ")) # Recebendo input do usuário
 	except:
-		print(f"Deve ser informado um número entre 0 e 10 para a nota da Atividade {contador_notas}!")
+		print(f"Deve ser informado um número entre 0 e 10 para a nota da Atividade {contador_notas}!") # Forncecendo mensagem de erro
 		exit()
-	if nota <= 10 and nota >=0:
+	if nota <= 10 and nota >=0: # Condições para os valores limites da nota
 		total += nota
 	else:
-		print(f"A nota da Atividade {contador_notas} deve ser um valor entre 0 e 10!")
+		print(f"A nota da Atividade {contador_notas} deve ser um valor entre 0 e 10!") # Fornecendo mensagem de erro
 		exit()
 
-media_disciplina = total / 10
+media_disciplina = total / 10 # Calculando media da disciplina
 
-print(f"A média final da disciplina é: {media_disciplina}")
+print(f"A média final da disciplina é: {media_disciplina}") # Fornecendo o valor de média como output ao usuário
